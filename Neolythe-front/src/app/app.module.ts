dc2f9ea25d7b2ac066cbb5a16c590a40758f0781
Nicolas <nicolas.gauss.1@orange.fr>
2020-10-28 17:40:14 +0100

    end dev side bar + navigation & style

--- Neolythe-front/src/app/app.module.ts
+++ Neolythe-front/src/app/app.module.ts
@@ -2,16 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { LeftNavbarComponent } from './left-navbar/left-navbar.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftNavbarComponent
+    LeftNavbarComponent,
+    AboutComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
