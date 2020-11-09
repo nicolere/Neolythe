d281f685eb22f7e123d3c4cc499e649c2c149e45
Nicolas <nicolas.gauss.1@orange.fr>
2020-11-09 21:26:15 +0100

    refacto name class + code left sideBar & responsiv

--- Neolythe-front/src/app/left-navbar/left-navbar.component.ts
+++ Neolythe-front/src/app/left-navbar/left-navbar.component.ts
@@ -1,13 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 
+interface CustomLinks {
+  routerLinkActive: boolean;
+  routerLink: string;
+  iconName: string;
+  linkName: string;
+}
+
 @Component({
   selector: 'app-left-navbar',
   templateUrl: './left-navbar.component.html',
   styleUrls: ['./left-navbar.component.scss']
 })
 export class LeftNavbarComponent implements OnInit {
+  public arrayLinks: CustomLinks[] = [];
 
-  constructor() { }
+  constructor() {
+    this.arrayLinks = [
+      {
+        routerLinkActive: true,
+        routerLink: '/dashboard',
+        iconName: 'dashboard',
+        linkName: 'Dashboard'
+      },
+      {
+        routerLinkActive: false,
+        routerLink: '#',
+        iconName: 'pie_chart',
+        linkName: 'Charts'
+      },
+      {
+        routerLinkActive: false,
+        routerLink: '#',
+        iconName: 'attach_money',
+        linkName: 'Crypto'
+      },
+      {
+        routerLinkActive: false,
+        routerLink: '#',
+        iconName: 'folder',
+        linkName: 'Projects'
+      },
+      {
+        routerLinkActive: true,
+        routerLink: '/about',
+        iconName: 'info',
+        linkName: 'About'
+      }
+    ]
+   }
 
   ngOnInit(): void {
   }
