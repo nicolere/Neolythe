c496af4870d5d9151877ff8231c125dcc75e4b3d
Nicolas GAU <nicolas.gauss.1@orange.fr>
2021-01-08 21:58:15 +0100

    refacto left navbar component + mock data

--- Neolythe-front/src/app/left-navbar/left-navbar.component.ts
+++ Neolythe-front/src/app/left-navbar/left-navbar.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-interface CustomLinks {
-  routerLinkActive: boolean;
-  routerLink: string;
-  iconName: string;
-  linkName: string;
-}
+import { NAVIGATION_LINKS } from './mock-nav-links'
 
 @Component({
   selector: 'app-left-navbar',
@@ -13,42 +7,10 @@ interface CustomLinks {
   styleUrls: ['./left-navbar.component.scss']
 })
 export class LeftNavbarComponent implements OnInit {
-  public arrayLinks: CustomLinks[] = [];
 
-  constructor() {
-    this.arrayLinks = [
-      {
-        routerLinkActive: true,
-        routerLink: '/dashboard',
-        iconName: 'dashboard',
-        linkName: 'Dashboard'
-      },
-      {
-        routerLinkActive: false,
-        routerLink: '#',
-        iconName: 'pie_chart',
-        linkName: 'Charts'
-      },
-      {
-        routerLinkActive: false,
-        routerLink: '#',
-        iconName: 'attach_money',
-        linkName: 'Crypto'
-      },
-      {
-        routerLinkActive: false,
-        routerLink: '#',
-        iconName: 'folder',
-        linkName: 'Projects'
-      },
-      {
-        routerLinkActive: true,
-        routerLink: '/about',
-        iconName: 'info',
-        linkName: 'About'
-      }
-    ]
-   }
+  public navigationLinks = NAVIGATION_LINKS;
+
+  constructor() {}
 
   ngOnInit(): void {
   }
