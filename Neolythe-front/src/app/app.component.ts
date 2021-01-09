0b39985169d8ce2779efadf47771d11a5c97ae25
Nicolas GAU <nicolas.gauss.1@orange.fr>
2021-01-09 21:08:09 +0100

    refacto structure design + responsiv with material

--- Neolythe-front/src/app/app.component.ts
+++ Neolythe-front/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { routeTransitionAnimations } from './route-animations-transitions';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
+import { distinctUntilChanged, tap, map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +11,42 @@ import { routeTransitionAnimations } from './route-animations-transitions';
   styleUrls: ['./app.component.scss'], 
   animations: [routeTransitionAnimations]
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
 
-  constructor() {}
+  watcher: Subscription;
+  activeMediaQuery: any = [];
+
+  public isSmallDevice: boolean = false;
+  public currentNavigationPath: string = "";
+  
+  constructor(private mediaObserver: MediaObserver, private router: Router) {
+    this.watcher = mediaObserver.asObservable()
+    .pipe(
+      distinctUntilChanged((prev: MediaChange[], curr: MediaChange[]) => prev[0].mqAlias === curr[0].mqAlias),
+      map((arr: MediaChange[]) => arr.map((change: MediaChange) => change.mqAlias)),
+      tap(change => this.activeMediaQuery.push(change)),
+    )
+    .subscribe((changes: string[]) => {
+      changes.includes('sm') || changes.includes('xs') 
+      ? this.isSmallDevice = true
+      : this.isSmallDevice = false   
+    });
+
+    router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    )
+    .subscribe((event: NavigationEnd) => {
+      console.log(event.url)
+      this.currentNavigationPath = event.url;
+    })
+  }
 
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animationState'];
+  }
+
+  ngOnDestroy(): void {
+    this.watcher.unsubscribe();
   }
 }
