85691cd9b8dcac1b35b9dff3cf2dce448d27bef9
Nicolas GAU <nicolas.gauss.1@orange.fr>
2021-01-31 22:45:30 +0100

    add projetc description + clean useless mock

--- Neolythe-front/src/app/about/mock-about-project.ts
+++
@@ -1,9 +0,0 @@
-interface AboutProject {
-    title: string;
-    description: string;
-}
-
-export const NEOLYTHE_ABOUT_PROJECT: AboutProject = {
-    title: 'Neolythe ?',
-    description: 'Projet Personnel en cours de développement ...'
-}
