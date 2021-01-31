8fed94e29986c65c10b304afc3d8dd4f4e7ec934
Nicolas GAU <nicolas.gauss.1@orange.fr>
2021-01-31 19:19:45 +0100

    start refacto 'about' section + mock data

--- Neolythe-front/src/app/about/mock-collaborators.ts
+++ Neolythe-front/src/app/about/mock-collaborators.ts
@@ -1,23 +1,57 @@
 export interface Collaborator {
+  picture: string;
   name: string;
   description: string;
-  links: string;
+  links: SocialLink[];
 }
 
+interface SocialLink {
+  name: string;
+  icon: string;
+  url: string;
+}
+
+// TODO : Ajuster les liens et img
 export const PROJECT_COLLABORATORS: Collaborator[] = [
   {
-    name: 'Adem',
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum nisl quis ante sagittis faucibus. Integer.",
-    links: 'test'
-  },
-  {
-    name: 'Sadok',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum nisl quis ante sagittis faucibus.',
-    links: 'test'
-  },
-  {
+    picture: './../../assets/img/profile-pictures/nicolas-picture.jpg',
     name: 'Nicolas',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum nisl quis ante sagittis faucibus. Vivamus luctus vehicula molestie. Integer..',
-    links: 'test'
-  }
+    description: 'FullStack Web Developer',
+    links: [
+      {
+        name: 'Email',
+        icon: 'email',
+        url: 'mailto:nicolas.gauss.1@orange.fr'
+      },
+      {
+        name: 'Github',
+        icon: './../../assets/img/icons/github.svg',
+        url: 'https://github.com/nicolere'
+      }
+    ]
+  },
+  {
+    picture: './../../assets/img/profile-pictures/sadok-picture.jpg',
+    name: 'Sadok',
+    description: 'Back-End Web Developer',
+    links: [
+      {
+        name: 'Email',
+        icon: 'email',
+        url: 'mailto:sadok.benfredj@gmail.com'
+      }
+    ]
+  },
+  {
+    picture: './../../assets/img/profile-pictures/adem-picture.jpg',
+    name: 'Adem',
+    description: "Fullstack Web Developer",
+    links: [
+      {
+        name: 'Email',
+        icon: 'email',
+        url: 'mailto:adem.gurbuz@gmail.com'
+      }
+    ]
+  },
 ];
