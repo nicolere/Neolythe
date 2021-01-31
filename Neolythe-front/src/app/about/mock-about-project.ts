6eaaf0270f98d617b6c7c2d2e9ca43bb83b53e74
Nicolas GAU <nicolas.gauss.1@orange.fr>
2021-01-31 19:19:45 +0100

    start refacto 'about' section + mock data

---
+++ Neolythe-front/src/app/about/mock-about-project.ts
@@ -0,0 +1,9 @@
+interface AboutProject {
+    title: string;
+    description: string;
+}
+
+export const NEOLYTHE_ABOUT_PROJECT: AboutProject = {
+    title: 'Neolythe ?',
+    description: 'Projet Personnel en cours de développement ...'
+}
