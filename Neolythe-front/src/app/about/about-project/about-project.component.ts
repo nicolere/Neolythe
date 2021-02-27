5da53af1d4fdcb7166ef5fb274c7eaef591b41e4
Nicolas GAU <nicolas.gauss.1@orange.fr>
2021-02-27 17:53:15 +0100

    clean & rename class + add transition

--- Neolythe-front/src/app/about/about-project/about-project.component.ts
+++ Neolythe-front/src/app/about/about-project/about-project.component.ts
@@ -1,20 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, AfterViewInit, ViewChild} from '@angular/core';
 import { PROJECT_COLLABORATORS } from './../mock-collaborators';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-about-project',
   templateUrl: './about-project.component.html',
   styleUrls: ['./about-project.component.scss']
 })
-export class AboutProjectComponent implements OnInit {
+export class AboutProjectComponent implements AfterViewInit {
 
   @Input() isSmallDevice: boolean;
+  @ViewChild('divDescription', {static: false}) el: ElementRef<HTMLDivElement>;
 
   public projectCollaborators = PROJECT_COLLABORATORS;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    gsap.from(this.el.nativeElement.children, {
+      delay: 1,
+      autoAlpha: 0,
+      y: -20,
+      stagger: 0.10
+    })
   }
 
 }
